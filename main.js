fb89f909dd9d1685b21a5cd38575ce079f76c046
feffe1702 <fpm.asd@gmail.com>
2024-10-08 14:21:40 +0200

    [WIP] add on/off function to botton

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
 const lampada = document.getElementById('imgLampada');
 const pulsante = document.getElementById('btnOnOff');
+
+// console.log(lampada , pulsante);
+
+const imgAccesa = './img/yellow_lamp.png';  // Immagine con lampada accesa
+const imgSpenta = './img/white_lamp.png';   // Immagine con lampada spenta
+
+pulsante.addEventListener("click", () => {
+    if (pulsante.textContent === "Accendi") {
+        pulsante.textContent = "Spegni";
+    }
+    else {
+            pulsante.textContent = "Accendi";
+    }
+})
