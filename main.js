6fe2cf06dafe5e01365ee72a041d54a8020ff421
feffe1702 <fpm.asd@gmail.com>
2024-10-08 14:47:09 +0200

    [DONE] add on/off animation to the lamp

--- main.js
+++ main.js
@@ -3,14 +3,16 @@ const pulsante = document.getElementById('btnOnOff');
 
 // console.log(lampada , pulsante);
 
-const imgAccesa = './img/yellow_lamp.png';  // Immagine con lampada accesa
-const imgSpenta = './img/white_lamp.png';   // Immagine con lampada spenta
+const imgAccesa = './img/yellow_lamp.png';
+const imgSpenta = './img/white_lamp.png';
 
 pulsante.addEventListener("click", () => {
     if (pulsante.textContent === "Accendi") {
         pulsante.textContent = "Spegni";
+        lampada.src = imgAccesa;
     }
     else {
             pulsante.textContent = "Accendi";
+            lampada.src = imgSpenta;
     }
 })
